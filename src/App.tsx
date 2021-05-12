8a85d79943bdc728af7e09db8bc74f7b4c1065ec
ptrickd <patrickdion8@gmail.com>
2021-05-12 13:32:46 -0600

    api working

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { fetchQuizQuestions } from './API';
+//Components
+import QuestionCard from './components/QuestionCard';
+//Types
+import { QuestionState, Difficulty } from './API';
 
-function App() {
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
+
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  // console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+
+  const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+
+  const nextQuestion = () => {
+
+  }
+
   return (
     <div className="App">
-      Quiz
+      <h1>REACT QUIZ</h1>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className='start' onClick={startTrivia}>
+          Start
+        </button>
+      ) : null}
+
+      {!gameOver ? <p className='score'>Score: </p> : null}
+      {loading && <p className=''>Loading Questions ...</p>}
+      {!loading && !gameOver && (
+        <QuestionCard
+          questionNum={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+      {!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
+        <button className='next' onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
+
     </div>
   );
 }
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
-const QuestionCard = () => {
-    <div>Questoin Card</div>
+type Props = {
+    question: string;
+    answers: string[];
+    callback: any;
+    userAnswer: any;
+    questionNum: number;
+    totalQuestions: number;
 }
+
+const QuestionCard: React.FC<Props> = ({
+    question,
+    answers,
+    callback,
+    userAnswer,
+    questionNum,
+    totalQuestions
+}) => (
+    <div>
+        <p className='number'>
+            Question: {questionNum}/{totalQuestions}
+        </p>
+        <p dangerouslySetInnerHTML={{ __html: question }} />
+        <div>
+            {answers.map(answer => (
+                <div>
+                    <button disabled={userAnswer} onClick={callback}>
+                        <span dangerouslySetInnerHTML={{ __html: answer }} />
+                    </button>
+                </div>
+            ))}
+        </div>
+    </div>
+);
 
 export default QuestionCard;
